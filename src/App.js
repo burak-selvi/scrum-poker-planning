0bb3c401df397c882bc5c1070dde6a52a4f9cb58
Burak <burakselvi858@hotmail.com>
2020-08-13 20:45:48 +0300

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+import { Layout } from "./components";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes />
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import {
+  AddStoryList,
+  ViewAsScrumMaster,
+  ViewAsDeveloper,
+  Error,
+} from "./pages";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/add-story-list" />
+      <Route exact path="/add-story-list" component={AddStoryList} />
+      <Route exact path="/view-planning-as-scrum-master" component={ViewAsScrumMaster} />
+      {/* <Route exact path="/view-planning-as-scrum-master/:sessionName" component={ViewAsScrumMaster} /> */}
+      <Route exact path="/view-planning-as-developer" component={ViewAsDeveloper} />
+      {/* <Route exact path="/view-planning-as-developer/:sessionName" component={ViewAsDeveloper} /> */}
+      <Route path="/error" component={Error} />
+      <Redirect to="/error" />
+    </Switch>
+  );
+}
