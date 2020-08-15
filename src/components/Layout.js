5689efb9702df67cf6d03c28407d8c6cc25d6140
Burak <burakselvi858@hotmail.com>
2020-08-15 17:19:31 +0300

    layout updated with master check and dev link

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,45 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { withRouter } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Container, Typography, Link, Box } from "@material-ui/core";
+import { projectAuth } from './../firebaseConfig';
 import Logo from './Logo';
+import { setUser } from "../actions";
+import { setLink } from './../actions';
+import { getMasterId, getDeveloperLink } from "../utils";
 
-function Layout({ children }) {
+export default function Layout({ children }) {
+  const dispatch = useDispatch();
   const urlLink = useSelector(state => state.link);
-  const pathName = window.location.pathname;
+  const { userId } = useSelector(state => state.user);
+  const link = getDeveloperLink();
+  const master = getMasterId();
+
+  useEffect(() => {
+    projectAuth.signInAnonymously();
+    projectAuth.onAuthStateChanged(firebaseUser => {
+      let isMaster;
+      if (!userId) {
+        if (master === firebaseUser?.uid) {
+          isMaster = true;
+        } else {
+          isMaster = false;
+        }
+        dispatch(setUser({ userId: firebaseUser?.uid, isMaster }));
+      }
+    });
+  }, [userId, master, dispatch]);
+
+  useEffect(() => {
+    if (!urlLink) {
+      dispatch(setLink(link));
+    }
+  }, [urlLink, link, dispatch]);
 
   return (
     <Container style={{ marginBottom: '5rem' }}>
       <Box display="flex" justifyContent="space-between" marginY="4rem">
         <Logo />
-        {pathName.includes('scrum-master') && urlLink &&
+        {!!master && urlLink &&
           <Typography variant="body2">
             please share link of developers panel to the teammates: <Link href={urlLink} >{urlLink}
             </Link>
@@ -22,4 +49,3 @@ function Layout({ children }) {
     </Container>
   )
 }
-export default withRouter(Layout);
