8c24805574af1008d44d747ef0bb437248828980
Burak <burakselvi858@hotmail.com>
2020-08-15 17:18:28 +0300

    developer page updated

--- src/components/DeveloperView.js
+++ src/components/DeveloperView.js
@@ -1,83 +1,98 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Grid, makeStyles } from '@material-ui/core';
 import { StoryList, ActiveStory } from '.';
-import { projectAuth, projectFirestore } from './../firebaseConfig';
+import { projectFirestore } from './../firebaseConfig';
 import { useParams } from 'react-router-dom';
+import { setSession } from '../actions';
 
 export default function DeveloperView() {
+  const dispatch = useDispatch();
   const classes = useStyles();
   const { sessionName } = useParams();
-  const [currentStory, setCurrentStory] = useState();
-  const [userId, setUserId] = useState('');
+  const session = useSelector(state => state.sessionName)
+  const { userId } = useSelector(state => state.user);
+  const [currentStory, setCurrentStory] = useState({});
+  const [currentSession, setCurrentSession] = useState({});
+  const [currentVote, setCurrentVote] = useState('');
   const [stories, setStories] = useState([]);
-  const [activeStory, setActiveStory] = useState({});
+  const [storyVotes, setStoryVotes] = useState([]);
+  const [activeStory, setActiveStory] = useState(null);
   const [statuses, setStatuses] = useState([]);
-  console.log('Session Name', sessionName)
-  // console.log(userId)
-  console.log('Active Story', activeStory)
+  const voteCheck = storyVotes.find(vote => vote.id === userId) || storyVotes.length < currentSession.votersNumber - 1;
 
   useEffect(() => {
-    let isMounted = true;
-    if (isMounted) {
-      projectAuth.signInAnonymously();
-      projectAuth.onAuthStateChanged(firebaseUser => {
-        setUserId(firebaseUser?.uid)
-      });
+    if (!session) {
+      dispatch(setSession(sessionName))
     }
-    return () => {
-      isMounted = false;
-      // projectAuth.signOut();
-    }
-  }, []);
+  }, [session, sessionName, dispatch]);
 
   useEffect(() => {
-    projectFirestore.collection(`sessions/${sessionName}/stories`).orderBy('position').onSnapshot(collection => {
-      const data = collection.docs.map(doc => {
-        if (doc.data().status === 2) {
-          setActiveStory(doc.data());
-        }
-        return {
-          ...doc.data(),
-          id: doc.id
-        }
+    if (session) {
+      projectFirestore.collection(`sessions/${session}/stories`).orderBy('position').onSnapshot(collection => {
+        const data = collection.docs.map(doc => {
+          if (doc.data().status === 2) {
+            setActiveStory(doc.data());
+          }
+          return {
+            ...doc.data(),
+            id: doc.id
+          }
+        });
+        setStories([...data]);
       });
-      setStories([...data]);
-    });
-    projectFirestore.collection(`statuses`).onSnapshot(collection => {
-      const data = collection.docs.map(doc => {
-        return {
-          ...doc.data(),
-          id: doc.id
-        }
+      projectFirestore.collection(`statuses`).onSnapshot(collection => {
+        const data = collection.docs.map(doc => {
+          return {
+            ...doc.data(),
+            id: doc.id
+          }
+        });
+        setStatuses([...data]);
       });
-      setStatuses([...data]);
-    });
-    projectFirestore.collection(`sessions`).where('name', '==', sessionName).onSnapshot(collection => {
-      const data = collection.docs.map(doc => {
-        return {
-          ...doc.data()
-        }
-      });
-      // console.log(data)
-    })
-  }, [sessionName]);
+      projectFirestore.collection(`sessions`).where('name', '==', session).onSnapshot(collection => {
+        const data = collection.docs.map(doc => {
+          return {
+            ...doc.data()
+          }
+        });
+        setCurrentSession(data[0]);
+      })
+    }
 
-  // const handleClick = () => {
-  //   projectFirestore.doc(`sessions/${sessionName}/stories/story1/votes/${userId}`).set({
-  //     name: `voter-${userId}`,
-  //     point: 90
-  //   })
-  // };
+  }, [session]);
+
+  useEffect(() => {
+    if (activeStory?.name) {
+      projectFirestore.collection(`sessions/${session}/stories/${activeStory.name}/votes`).onSnapshot(collection => {
+        const data = collection.docs.map(doc => {
+          if (doc.id === userId) {
+            setCurrentVote(doc.data().point);
+          }
+          return {
+            ...doc.data(),
+            id: doc.id
+          }
+        });
+        setStoryVotes([...data]);
+      });
+    }
+  }, [activeStory, session, userId]);
+
 
   return (
-    <Grid container>
-      <Grid item xs={7} >
-        <StoryList statuses={statuses} stories={stories} currentStory={currentStory} setCurrentStory={setCurrentStory} />
-      </Grid>
-      <Grid item xs={4} className={classes.ml5}>
-        <ActiveStory />
-      </Grid>
-    </Grid>
+    <React.Fragment>
+      {voteCheck ?
+        <Grid container>
+          <Grid item xs={7} >
+            <StoryList statuses={statuses} stories={stories} currentStory={currentStory} setCurrentStory={setCurrentStory} />
+          </Grid>
+          {activeStory && <Grid item xs={4} className={classes.ml5}>
+            <ActiveStory storyVotes={storyVotes} votersNumber={currentSession.votersNumber} currentVote={currentVote} activeStory={activeStory} />
+          </Grid>}
+        </Grid> : <p>No Voting</p>
+      }
+    </React.Fragment>
   )
 }
 
