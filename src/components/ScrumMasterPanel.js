378a00cc1af8be68f8f1c889a0294f0535e45a0f
Burak <burakselvi858@hotmail.com>
2020-08-15 17:18:20 +0300

    scrum master panel pages updated

--- src/components/ScrumMasterPanel.js
+++ src/components/ScrumMasterPanel.js
@@ -1,40 +1,97 @@
-import React from 'react'
-import { Box, Typography, TextField, Button } from '@material-ui/core';
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { Box, Typography, TextField, Button, makeStyles } from '@material-ui/core';
+import { projectFirestore } from '../firebaseConfig';
 
-export default function ScrumMasterPanel() {
+export default function ScrumMasterPanel({ storyVotes, votersNumber, activeStory, stories }) {
+  const sessionName = useSelector(state => state.sessionName);
+  const [finalScore, setFinalScore] = useState('');
+  const classes = useStyles();
+  let isAllVoted = false;
+
+  const createVoters = (count) => {
+    let arr = [];
+    for (let index = 0; index < count - 1; index++) {
+      let finalPoint = 'Not Voted';
+      let point = storyVotes.find(voter => voter.position === index + 1)?.point;
+      isAllVoted = storyVotes.some(voter => voter.isLastVote);
+      if (point) {
+        if (isAllVoted) {
+          finalPoint = point;
+        } else {
+          finalPoint = 'Voted'
+        }
+      }
+      arr[index] = { name: `Voter ${index + 1} : `, position: index + 1, point: finalPoint };
+    }
+    const point = storyVotes.find(voter => voter.position === count)?.point;
+    let finalPoint = 'Not Voted';
+    if (point) {
+      if (isAllVoted) {
+        finalPoint = point;
+      } else {
+        finalPoint = 'Voted';
+      }
+    }
+    arr[count - 1] = { name: `Scrum Master : `, position: count, point: finalPoint };
+    return arr;
+  }
+
+  const handleFinalScoreChange = event => {
+    setFinalScore(parseInt(event.target.value));
+  }
+
+  const handleEndVoting = () => {
+    const nextStory = stories.find(story => story.position === activeStory.position + 1);
+    projectFirestore.doc(`sessions/${sessionName}/stories/${activeStory.id}`).update({
+      point: finalScore,
+      status: 1
+    });
+    if (nextStory) {
+      projectFirestore.doc(`sessions/${sessionName}/stories/${nextStory.id}`).update({
+        point: 0,
+        status: 2
+      });
+    }
+    setFinalScore('');
+  }
+
   return (
     <React.Fragment>
       <p>Scrum Master Panel</p>
       <Box border='1px solid black' height='100%'>
         <Box padding="1rem">
-          <Typography variant="body2">
-            Story 1 is Active
-        </Typography>
-          <Typography variant="body2">
-            Voter 1 : 13
-        </Typography>
-          <Typography variant="body2">
-            Voter 1 : 13
-        </Typography>
-          <Typography variant="body2">
-            Voter 1 : 13
-        </Typography>
-          <Typography variant="body2">
-            Voter 1 : 13
-        </Typography>
-          <Typography variant="body2">
-            Scrum Master : 15
-        </Typography>
+          <Typography variant="body2" className={classes.mb3}>
+            {activeStory?.name} is Active
+          </Typography>
+          {createVoters(votersNumber).map(voter => {
+            return (
+              <Typography key={voter.name} variant="body2">
+                {voter.name} {voter.point}
+              </Typography>
+            )
+          })}
         </Box>
         <Box display="flex" flexDirection="column" alignItems="center" marginTop="3rem" paddingLeft="2rem" paddingRight="2rem">
-          <Typography variant="caption">
-            Seems teams has different votes
+          {isAllVoted && <React.Fragment>
+            <Typography variant="caption">
+              Seems teams has different votes
         </Typography>
-          <Typography variant="caption" style={{ textAlign: 'center' }}>
-            Please discuss and finalize the score below textbox
+            <Typography variant="caption" style={{ textAlign: 'center' }}>
+              Please discuss and finalize the score below textbox
         </Typography>
-          <TextField variant="outlined" label="Final Score" style={{ marginTop: '1rem' }} />
-          <Button variant="outlined" style={{ marginTop: '1rem' }}>
+          </React.Fragment>}
+          <TextField
+            value={finalScore}
+            onChange={handleFinalScoreChange}
+            variant="outlined"
+            label="Final Score"
+            style={{ marginTop: '1rem' }} />
+          <Button
+            onClick={handleEndVoting}
+            variant="outlined"
+            style={{ marginTop: '1rem' }}
+            disabled={!isAllVoted}>
             End Voting For Story 1
           </Button>
         </Box>
@@ -42,3 +99,9 @@ export default function ScrumMasterPanel() {
     </React.Fragment>
   )
 }
+
+const useStyles = makeStyles(theme => ({
+  mb3: {
+    marginBottom: theme.spacing(3),
+  }
+}));
--- src/components/ScrumMasterView.js
+++ src/components/ScrumMasterView.js
@@ -1,21 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Grid, makeStyles } from '@material-ui/core';
 import { StoryList, ActiveStory, ScrumMasterPanel } from '.';
+import { useParams } from 'react-router-dom';
+import { projectFirestore } from './../firebaseConfig';
+import { setSession } from '../actions';
 
 export default function ScrumMasterView() {
+  const dispatch = useDispatch();
+  const { sessionName } = useParams();
   const classes = useStyles();
+  const session = useSelector(state => state.sessionName)
+  const { userId } = useSelector(state => state.user);
+  const [currentSession, setCurrentSession] = useState({});
+  const [currentVote, setCurrentVote] = useState('');
+  const [stories, setStories] = useState([]);
+  const [storyVotes, setStoryVotes] = useState([]);
+  const [activeStory, setActiveStory] = useState(null);
+  const [statuses, setStatuses] = useState([]);
+
+  useEffect(() => {
+    if (!session) {
+      dispatch(setSession(sessionName))
+    }
+  }, [session, sessionName, dispatch]);
+
+  useEffect(() => {
+    if (session) {
+      projectFirestore.collection(`sessions/${session}/stories`).orderBy('position').onSnapshot(collection => {
+        const data = collection.docs.map(doc => {
+          if (doc.data().status === 2) {
+            setActiveStory({ ...doc.data(), id: doc.id });
+          }
+          return {
+            ...doc.data(),
+            id: doc.id
+          }
+        });
+        setStories([...data]);
+      });
+      projectFirestore.collection(`statuses`).onSnapshot(collection => {
+        const data = collection.docs.map(doc => {
+          return {
+            ...doc.data(),
+            id: doc.id
+          }
+        });
+        setStatuses([...data]);
+      });
+      projectFirestore.collection(`sessions`).where('name', '==', session).onSnapshot(collection => {
+        const data = collection.docs.map(doc => {
+          return {
+            ...doc.data()
+          }
+        });
+        setCurrentSession(data[0]);
+      })
+    }
+
+  }, [session]);
+
+  useEffect(() => {
+    if (activeStory?.name) {
+      projectFirestore.collection(`sessions/${session}/stories/${activeStory.name}/votes`).orderBy('position').onSnapshot(collection => {
+        const data = collection.docs.map(doc => {
+          if (doc.id === userId) {
+            setCurrentVote(doc.data().point);
+          }
+          return {
+            ...doc.data(),
+            id: doc.id
+          }
+        });
+        setStoryVotes([...data]);
+      });
+    }
+  }, [activeStory, session, userId]);
 
   return (
     <Grid container>
       <Grid item xs={5} >
-        <StoryList />
+        <StoryList statuses={statuses} stories={stories} />
       </Grid>
-      <Grid item xs={3} className={classes.ml3}>
-        <ActiveStory />
-      </Grid>
-      <Grid item xs={3} className={classes.ml5}>
-        <ScrumMasterPanel />
-      </Grid>
+      {activeStory && <React.Fragment>
+        <Grid item xs={3} className={classes.ml3}>
+          <ActiveStory storyVotes={storyVotes} votersNumber={currentSession.votersNumber} currentVote={currentVote} activeStory={activeStory} />
+        </Grid>
+        <Grid item xs={3} className={classes.ml5}>
+          <ScrumMasterPanel stories={stories} activeStory={activeStory} storyVotes={storyVotes} votersNumber={currentSession.votersNumber} />
+        </Grid>
+      </React.Fragment>}
     </Grid>
   )
 }
